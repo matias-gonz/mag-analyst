77bcc3482612b08f694583806802c915cbe5bae0
Matías Ignacio González <maigonzalez@fi.uba.ar>
2022-12-28 12:19:55 -0300

    Add log buttons for plots

--- Plotter.m
+++ Plotter.m
@@ -35,44 +35,56 @@ classdef Plotter
             obj.Hcr = Hcr;
         end
 
-        function plot_Hcr(obj)
+        function plot_Hcr(obj, ax)
             for i = 1:length(obj.Hcr)
-                xline(obj.Hcr(i), '--', "Color",[0, 0.4470, 0.7410], "Label", "Hcr");
+                xline(ax, obj.Hcr(i), '--', "Color",[0, 0.4470, 0.7410], "Label", "Hcr");
             end
         end
 
         function plot_M(obj, ax)
+
+            hold( ax, 'on' )
             plot(ax, obj.H, obj.M, '.', 'markersize', obj.MarkerSize, "Color",[0, 0, 0]);
-            %hold on;
             plot(ax, obj.Hhat, obj.Mhat, "Color",[0,0,0]);
-            %obj.plot_Hcr;
+            obj.plot_Hcr(ax)
             xlabel(ax, 'H (A/m)');
             ylabel(ax, 'M (A/m)');
-            %hold off;
+            hold( ax, 'off' )     
         end
 
-        function plot_M_log(obj, ~)
-            semilogx(obj.H,obj.M, '.', 'markersize', obj.MarkerSize, "Color",[0, 0, 0]);
-            hold on;
-            semilogx(obj.Hhat,obj.Mhat, "Color",[0 0 0]);
-            obj.plot_Hcr;
-            hold off;
-            xlabel('H (A/m)');
-            ylabel('M (A/m)');
+        function plot_M_log(obj, ax)
+            semilogx(ax, obj.H,obj.M, '.', 'markersize', obj.MarkerSize, "Color",[0, 0, 0]);
+            hold( ax, 'on' )
+            semilogx(ax, obj.Hhat,obj.Mhat, "Color",[0 0 0]);
+            xlabel(ax, 'H (A/m)');
+            ylabel(ax, 'M (A/m)');
+            obj.plot_Hcr(ax);
+            hold( ax, 'off' )
         end
 
         function plot_dMdH(obj, ax)
-            %hold on;
+            hold( ax, 'on' );
             plot(ax, obj.H,obj.dMdH, '.', 'markersize', obj.MarkerSize, "Color",[0, 0, 0]);
             plot(ax, obj.Hhat, obj.dMdHhat, "Color",[0 0 0]);
-            %obj.plot_Hcr;
+            obj.plot_Hcr(ax);
             xlabel(ax, 'H (A/m)');
             ylabel(ax, '∂M/∂H');
             ax.YLim = [min(obj.dMdH) max(obj.dMdH)*1.1];
-            %hold off;
+            hold( ax, 'off' )
         end
 
-        function plot_HdMdH(obj, ax)
+        function plot_dMdH_log(obj, ax)
+            semilogx(ax, obj.H,obj.dMdH, '.', 'markersize', obj.MarkerSize, "Color",[0, 0, 0]);
+            hold( ax, 'on' );
+            semilogx(ax, obj.Hhat, obj.dMdHhat, "Color",[0 0 0]);
+            obj.plot_Hcr(ax);
+            xlabel(ax, 'H (A/m)');
+            ylabel(ax, '∂M/∂H');
+            %ax.YLim = [min(obj.dMdH) max(obj.dMdH)*1.1];
+            hold( ax, 'off' )
+        end
+
+        function plot_HdMdH_log(obj, ax)
             %semilogx(obj.H,obj.dMdH, '.', 'markersize', obj.MarkerSize, "Color",[0, 0, 0]);
             %hold on;
             %obj.plot_Hcr;
@@ -83,15 +95,26 @@ classdef Plotter
             %ylabel('∂M/∂H');
             
             %yyaxis right;
-            %hold on;
             semilogx(ax, obj.Hhat, obj.HdMdHhat, "Color",[0, 0, 0]);
+            hold( ax, 'on' );
             semilogx(ax, obj.H,obj.HdMdH, '.', 'markersize', obj.MarkerSize, "Color",[0, 0, 0]);
+            obj.plot_Hcr(ax);
             xlabel(ax, 'H (A/m)');
             ylabel(ax, 'H ∂M/∂H (A/m)', "Color",[0, 0, 0]);
-            %hold off;
+            hold( ax, 'off' );
 
             %yyaxis left;  ax.YLim = [0 max(obj.dMdH)*1.1];
             %yyaxis right; ax.YLim = [0 max(obj.HdMdHhat)*1.1];
+        end
+
+        function plot_HdMdH(obj, ax)
+            hold( ax, 'on' );
+            plot(ax, obj.Hhat, obj.HdMdHhat, "Color",[0, 0, 0]);
+            plot(ax, obj.H,obj.HdMdH, '.', 'markersize', obj.MarkerSize, "Color",[0, 0, 0]);
+            obj.plot_Hcr(ax);
+            xlabel(ax, 'H (A/m)');
+            ylabel(ax, 'H ∂M/∂H (A/m)', "Color",[0, 0, 0]);
+            hold( ax, 'off' );
         end
 
         function plot(obj)
