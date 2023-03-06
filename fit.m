9c2d79483ce4cc822ef6c7a8ef57d3142d46aab1
Matías Ignacio González <maigonzalez@fi.uba.ar>
2023-03-06 10:21:25 -0300

    Add number of components option

--- fit.m
+++ fit.m
@@ -1,6 +1,6 @@
 function [Hcr, mcr] = fit(H, M, seed)
 
-    options = optimset('PlotFcns',@optimplotfval);
+    %options = optimset('PlotFcns',@optimplotfval);
     [HTip, ~] = Utils().find_tip(H, M);
     H_log = log(H);
     N = 100;
@@ -8,12 +8,12 @@ function [Hcr, mcr] = fit(H, M, seed)
     function ret = fit_parameters(x)
         Hcr_fit = x(1:end/2);
         mcr_fit = x((end/2)+1:end);
-        for i = 1:length(mcr_fit)
-            if mcr_fit(i) < 0.44951
-                ret = 1000;
-                return;
-            end
-        end
+        %for i = 1:length(mcr_fit)
+        %    if mcr_fit(i) < 0.44951
+        %        ret = 1000;
+        %        return;
+        %    end
+        %end
         magnetic_parameters = MagneticParameters(H, M, Hcr_fit, mcr_fit);
         a = magnetic_parameters.get_a();
         alphaMs = magnetic_parameters.get_alphaMs(a);
@@ -25,7 +25,13 @@ function [Hcr, mcr] = fit(H, M, seed)
 
         ret = d_error;
     end
-    params = fminsearch(@fit_parameters, seed, options);
+    
+
+    %params = fminsearch(@fit_parameters, seed, options);
+    lb = [0.0001 0.44951];
+    ub = [inf 1];
+    params = minimize(@fit_parameters, seed, [],[], [],[], lb, ub);
+
     Hcr = params(1:end/2);
     mcr = params((end/2)+1:end);
 end
