2175e7bf5159620b1e754e1dbbddfa35514196dd
Matías Ignacio González <maigonzalez@fi.uba.ar>
2023-03-26 17:28:01 -0300

    Add error to minimize dropdown

--- ErrorCalculator.m
+++ ErrorCalculator.m
@@ -1,9 +1,34 @@
 classdef ErrorCalculator
-    methods
+    methods (Access = public)
         function obj = ErrorCalculator()
         end
 
+        function e = get_error(obj, X, Y, Xhat, Yhat, error_type)
+            switch error_type
+                case "Diagonal"
+                    e = obj.diagonal_error(X, Y, Xhat, Yhat);
+                case "Vertical"
+                    e = obj.vertical_error(X, Y, Xhat, Yhat);
+                case "Horizontal"
+                    e = obj.horizontal_error(X, Y, Xhat, Yhat);
+            end
+        end
+
+        function residue = residue(~, X, Y, Xhat, Yhat)
+            Yint = interp1(Xhat, Yhat, X);
+            Yint = Yint(3:end);
+            Ydat = Y(3:end);
+            residue = zeros(1, length(Ydat));
+            for i = 1:length(Yint)
+                residue(i) = Ydat(i) - Yint(i);
+            end
+        end
+    end
+
+    methods (Access = private)
         function d_error = diagonal_error(~, X, Y, Xhat, Yhat)
+            X = X(2:end);
+            Y = Y(2:end);
             Ydat = Y(2:end-1);
             Xdat = X(2:end-1);
             d_error = 0;
@@ -16,14 +41,28 @@ classdef ErrorCalculator
             d_error = sqrt(d_error)/length(Y);
         end
 
-        function residue = residue(~, X, Y, Xhat, Yhat)
+        function v_error = vertical_error(~, X, Y, Xhat, Yhat)
             Yint = interp1(Xhat, Yhat, X);
             Yint = Yint(3:end);
             Ydat = Y(3:end);
-            residue = zeros(1, length(Ydat));
+            v_error = 0;
             for i = 1:length(Yint)
-                residue(i) = Ydat(i) - Yint(i);
+                e = Ydat(i) - Yint(i);
+                v_error = v_error + (e^2);
             end
+            v_error = (sqrt(v_error)/max(Y))/length(Y);
+        end
+
+        function h_error = horizontal_error(~, X, Y, Xhat, Yhat)
+            Xint = interp1(Yhat, Xhat, Y);
+            Xint = Xint(2:end);
+            Xdat = X(2:end);
+            h_error = 0;
+            for i = 1:length(Xint)
+                e = Xdat(i) - Xint(i);
+                h_error = h_error + (e^2);
+            end
+            h_error = sqrt(h_error)/max(X)/length(Xdat);
         end
     end
 end
--- fit.m
+++ fit.m
@@ -1,4 +1,4 @@
-function [Hcr, mcr] = fit(H, M, seed, select_a)
+function [Hcr, mcr] = fit(H, M, seed, select_a, error_type)
     [HTip, ~] = Utils().find_tip(H, M);
     H_log = log(H);
     N = 100;
@@ -6,25 +6,18 @@ function [Hcr, mcr] = fit(H, M, seed, select_a)
     error = ErrorCalculator();
     function ret = fit_parameters(x)
         Hcr_fit = x(1:end/2);
-        mcr_fit = x((end/2)+1:end);
+        mcr_fit = x((end/2)+1:end); 
 
         magnetic_parameters = MagneticParameters(H, M, Hcr_fit, mcr_fit);
         a = magnetic_parameters.get_a(select_a);
         alphaMs = magnetic_parameters.get_alphaMs(a);
         Ms = magnetic_parameters.get_Ms(a, alphaMs);
         Mhat = get_Mhat(Hhat, a, alphaMs, Ms);
-        %[h_error, ~] = horizontal_error(H_log, M, log(Hhat), Mhat);
-        %[h_error, ~] = vertical_error(H_log, M, log(Hhat), Mhat);
-        d_error = error.diagonal_error(H_log(2:end), M(2:end), log(Hhat), Mhat);
-
-        ret = d_error;
+        ret = error.get_error(H_log, M, log(Hhat), Mhat, error_type);
     end
     
     lb = [repmat(0.0001, 1, length(seed)/2) repmat(0.44951, 1, length(seed)/2)];
     ub = [inf(1, length(seed)/2) ones(1, length(seed)/2)];
-    disp(seed)
-    disp(lb)
-    disp(ub)
     params = minimize(@fit_parameters, seed, [],[], [],[], lb, ub);
 
     Hcr = params(1:end/2);
