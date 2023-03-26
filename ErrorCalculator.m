12c1aade16f57deeae992056d1d10c7308cf59f7
Matías Ignacio González <maigonzalez@fi.uba.ar>
2023-03-26 17:04:17 -0300

    Fix residue

--- ErrorCalculator.m
+++ ErrorCalculator.m
@@ -16,14 +16,13 @@ classdef ErrorCalculator
             d_error = sqrt(d_error)/length(Y);
         end
 
-        function d_residue = diagonal_residue(~, X, Y, Xhat, Yhat)
-            Ydat = Y(2:end-1);
-            Xdat = X(2:end-1);
-            d_residue = zeros(1, length(Xdat));
-            for i = 1:length(Ydat)
-                delta_x = abs(interp1(Yhat, Xhat, Ydat(i)) - Xdat(i))/max(X);
-                delta_y = abs(interp1(X, Y, interp1(Yhat, Xhat, Ydat(i))) - Ydat(i))/max(Y);
-                d_residue(i) = delta_y*cos(atan(delta_y/delta_x));
+        function residue = residue(~, X, Y, Xhat, Yhat)
+            Yint = interp1(Xhat, Yhat, X);
+            Yint = Yint(3:end);
+            Ydat = Y(3:end);
+            residue = zeros(1, length(Ydat));
+            for i = 1:length(Yint)
+                residue(i) = Ydat(i) - Yint(i);
             end
         end
     end
