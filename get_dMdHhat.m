7d4458a1a091e673a5e337b1656c4993794d90a7
Matías Ignacio González <maigonzalez@fi.uba.ar>
2023-04-19 10:00:32 -0300

    Separate dMdHhat components

--- get_dMdHhat.m
+++ get_dMdHhat.m
@@ -1,11 +1,14 @@
-function dMdHhat = get_dMdHhat(H, alpha, Ms, a, alphaMs)
+function [dMdHhat, dMidHhat] = get_dMdHhat(H, alpha, Ms, a, alphaMs)
     dMdHhat = zeros(1, length(H));
+    dMidHhat = zeros(length(a), length(H));
+
     for i = 1:length(H)
         for j = 1:length(a)
             m = Utils().get_m(H(i), a(j), alphaMs(j));
             h = (H(i)+m*alphaMs(j))/a(j);
             numerator = Ms(j)*Langevin(h,1)/a(j);
-            dMdHhat(i) = dMdHhat(i) + numerator/(1-alpha(j)*numerator);
+            dMidHhat(j, i) = numerator/(1-alpha(j)*numerator);
+            dMdHhat(i) = dMdHhat(i) + dMidHhat(j, i);
         end  
     end
 end
