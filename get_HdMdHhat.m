1fe439ae2b817c48ef868c42dba84eca186702d7
Matías Ignacio González <maigonzalez@fi.uba.ar>
2023-04-23 22:50:01 -0300

    Fix dMdlogH hat calculation

--- get_HdMdHhat.m
+++ get_HdMdHhat.m
@@ -4,7 +4,7 @@ function [HdMdHhat, HdMidHhat] = get_HdMdHhat(H, dMidHhat)
 
     for i = 1:length(H)
         for j = 1:size(dMidHhat,1)
-            HdMidHhat(j, i) = H(i)*dMidHhat(j);
+            HdMidHhat(j, i) = H(i)*dMidHhat(j, i);
             HdMdHhat(i) = HdMdHhat(i) + HdMidHhat(j, i);
         end
     end
