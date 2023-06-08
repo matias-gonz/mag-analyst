84f4ee266745bed8e751fd5c8154a12460aac385
Tomas Francisco Gonzalez <tfgonzalez@fi.uba.ar>
2023-06-08 00:20:08 -0300

    Add support for Brext [Gauss] for horizontal axis

--- UnitConvertor.m
+++ UnitConvertor.m
@@ -5,10 +5,11 @@ classdef UnitConvertor
 
     methods
         function obj = UnitConvertor()
-            units = ["H [A/m]" "H [kA/m]" "H [Oe]" "H [kOe]" "Bext [T]"];
+            units = ["H [A/m]" "H [kA/m]" "H [Oe]" "H [kOe]" "Bext [T]" "Bext [Gauss]"];
             oe_conversion = 79.5774715459;
             bext_conversion = 1/(4*pi*10^-7);
-            conversion = [1 1000 oe_conversion oe_conversion*1000 bext_conversion];
+            bext_gauss_conversion = 1/(10000 * bext_conversion);
+            conversion = [1 1000 oe_conversion oe_conversion*1000 bext_conversion bext_gauss_conversion];
             obj.UnitConversions = dictionary(units, conversion);
         end
 
