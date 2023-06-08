5ce37b20e8d3d2017300ca2fc3366f99940fe8de
Tomas Francisco Gonzalez <tfgonzalez@fi.uba.ar>
2023-06-07 23:02:35 -0300

    Add support for H [kOe] for horizontal axis

--- UnitConvertor.m
+++ UnitConvertor.m
@@ -5,9 +5,9 @@ classdef UnitConvertor
 
     methods
         function obj = UnitConvertor()
-            units = ["H [A/m]" "H [kA/m]" "H [Oe]"];
+            units = ["H [A/m]" "H [kA/m]" "H [Oe]" "H [kOe]"];
             oe_conversion = 79.5774715459;
-            conversion = [1 1000 oe_conversion];
+            conversion = [1 1000 oe_conversion oe_conversion*1000];
             obj.UnitConversions = dictionary(units, conversion);
         end
 
