bf1758d286da60ad8f518311224abafa8a326fd0
Tomas Francisco Gonzalez <tfgonzalez@fi.uba.ar>
2023-06-09 00:13:25 -0300

    Add support for B [Gauss] for vertical axis

--- UnitConvertor.m
+++ UnitConvertor.m
@@ -5,11 +5,11 @@ classdef UnitConvertor
 
     methods
         function obj = UnitConvertor()
-            units = ["H [A/m]" "H [kA/m]" "H [Oe]" "H [kOe]" "Bext [T]" "Bext [Gauss]" "Bext [kGauss]" "M [A/m]" "M [kA/m]" "M [MA/m]" "M [emu/cm^3]" "J [T]" "B [T]"];
+            units = ["H [A/m]" "H [kA/m]" "H [Oe]" "H [kOe]" "Bext [T]" "Bext [Gauss]" "Bext [kGauss]" "M [A/m]" "M [kA/m]" "M [MA/m]" "M [emu/cm^3]" "J [T]" "B [T]" "B [Gauss]"];
             oe_conversion = 79.5774715459;
             tesla_conversion = 1/(4*pi*10^-7);
             gauss_conversion = 1/(10000 * tesla_conversion);
-            conversion = [1 1000 oe_conversion oe_conversion*1000 tesla_conversion gauss_conversion gauss_conversion*1000 1 1000 1000000 1000 tesla_conversion tesla_conversion];
+            conversion = [1 1000 oe_conversion oe_conversion*1000 tesla_conversion gauss_conversion gauss_conversion*1000 1 1000 1000000 1000 tesla_conversion tesla_conversion gauss_conversion];
             obj.UnitConversions = dictionary(units, conversion);
         end
 
--- app.m
+++ app.m
@@ -435,7 +435,7 @@ classdef app < matlab.apps.AppBase
             
             app.H = unit_convertor.convert(H_raw, H_unit);
             app.M = unit_convertor.convert(M_raw, M_unit);
-            if(M_unit == "B [T]")
+            if(M_unit == "B [T]" || M_unit == "B [Gauss]")
                 app.M = app.M - app.H;
             end
         end
@@ -744,7 +744,7 @@ classdef app < matlab.apps.AppBase
 
             % Create VerticalaxisfieldDropDown
             app.VerticalaxisfieldDropDown = uidropdown(app.GridLayoutInputVerticalAxis);
-            app.VerticalaxisfieldDropDown.Items = {'M [A/m]', 'M [kA/m]', 'M [MA/m]', 'M [emu/cm^3]', 'J [T]', 'B [T]'};
+            app.VerticalaxisfieldDropDown.Items = {'M [A/m]', 'M [kA/m]', 'M [MA/m]', 'M [emu/cm^3]', 'J [T]', 'B [T]', 'B [Gauss]'};
             app.VerticalaxisfieldDropDown.Layout.Row = 1;
             app.VerticalaxisfieldDropDown.Layout.Column = 2;
             app.VerticalaxisfieldDropDown.Value = 'M [A/m]';
